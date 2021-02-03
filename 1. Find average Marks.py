f5ebf048670975bb78e9f6e1cf183223e540177c
UTKARSH JINDAL <70633782+jindalutkarsh@users.noreply.github.com>
2021-02-03 22:57:18 +0530

    Create 1. Find average Marks.py

---
+++ 1. Find average Marks.py
@@ -0,0 +1,17 @@
+# Read input as sepcified in the question
+a=int(input())
+b=int(input())
+c=int(input())
+average=(a+b+c)/3
+
+
+
+
+
+
+
+# Print output as specified in the question
+
+
+
+print(average)
